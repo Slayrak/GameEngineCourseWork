9e8105e9783622f090614e189855d03c1be4b3ef
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-05-10 19:30:32 +0300

    Add key event definition

--- EngineCore/EngineCore/src/EngineCore/Events/KeyEvent.h
+++ EngineCore/EngineCore/src/EngineCore/Events/KeyEvent.h
@@ -1,1 +1,58 @@
 #pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace EngineCore 
+{
+
+	class ENGINE_API KeyEvent : public Event
+	{
+	protected:
+		KeyEvent(int keycode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	};
+
+	class ENGINE_API KeyPressedEvent : public KeyEvent
+	{
+	private:
+		int m_RepeatCount;
+	public:
+		KeyPressedEvent(const int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (number of repeats = " << m_RepeatCount << ")";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+	};
+
+	class ENGINE_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+}
